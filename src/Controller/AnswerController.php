0c709b99453746d85f625d9768dc3a5f0a5262b1
jdevine <joseph.devine.mt@gmail.com>
2022-01-09 15:53:26 -0700

    added popular answers page

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -3,18 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Answer;
+use App\Repository\AnswerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnswerController extends AbstractController
 {
     /**
+     * @Route("/answers/popular", name="app_popular_answers")
+     */
+    public function popularAnswers(AnswerRepository $answerRepository): Response
+    {
+        return $this->render('answer/popularAnswers.html.twig', [
+            'answers' => $answerRepository->findMostPopular(),
+        ]);
+    }
+
+    /**
      * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
      */
-    public function answerVote(Answer $answer, LoggerInterface $logger, Request $request, EntityManagerInterface $entityManager)
+    public function answerVote(Answer $answer, LoggerInterface $logger, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $direction = $data['direction'] ?? 'up';
