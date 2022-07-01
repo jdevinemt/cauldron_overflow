2e9f0e78b9c00cda581027464b08488fb7dfeaf2
jdevine <joseph.devine.mt@gmail.com>
2022-07-01 13:27:22 -0600

    added enable 2fa template which renders 2fa QR code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Endroid\QrCode\Builder\Builder;
 use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -43,6 +44,20 @@ class SecurityController extends BaseController
             $entityManager->flush();
         }
 
-        dd($totpAuthenticator->getQRContent($user));
+        return $this->render('security/enable2fa.html.twig');
+    }
+
+    /**
+     * @Route("/authenticate/2fa/qr-code", name="app_qr_code")
+     * @IsGranted("ROLE_USER")
+     */
+    public function displayGoogleAuthenticatorQrCode(TotpAuthenticatorInterface $totpAuthenticator): Response
+    {
+        $qrCodeContent = $totpAuthenticator->getQRContent($this->getUser());
+        $result = Builder::create()
+            ->data($qrCodeContent)
+            ->build();
+
+        return new Response($result->getString(), 200, ['Content-Type' => 'image/png']);
     }
 }
